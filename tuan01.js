df468ee1cadfef3060f4f6eaf04485cee62f5e2b
VietNguyenlk <viet1282002@gmail.com>
2023-09-17 17:49:02 +0700

    init

---
+++ tuan01.js
@@ -0,0 +1,14 @@
+var markMass;
+var johnMass;
+var markHeight;
+var johnHeight;
+
+var bmi
+
+function BMI(a, b) {
+    bmi = a / (b * b)
+    return bmi
+}
+
+BMI(78, 1.69)
+console.log(BMI(78, 1.69))
